3b2088b3e6c8e4f5494286b4d9190ce40c472124
Xingorno <xsw781634319@163.com>
2019-09-05 10:03:29 +0800

    Update HistogramEqualisation.m

--- HistogramEqualisation.m
+++ HistogramEqualisation.m
@@ -9,48 +9,37 @@
 % Output:
 %    processedImg: transfered image
 
+% Author: Shuwei Xing
+% Date: 2019-09-04
+
 function processedImg = HistogramEqualisation(img, low, high, method, alpha)
 
-if nargin < 3
-    
+if nargin < 3   
    error(message('Input parameters is not enough, please recheck.'))
-
 end
 
-if nargin == 3
-    
-    method = 1;
-    
+if nargin == 3    
+    method = 1;    
 end
 
-if method ==2 && nargin ==4
-    
-    alpha = 0.025
-    
+if method ==2 && nargin ==4    
+    alpha = 0.025;    
 end
 
 numElement = numel(img);
-
 uniqueElement = unique(img);
-
 numUniqueElement = size(uniqueElement, 1);
 
 % compute old probability function
 numPixelList = [];
-for i = 1 : numUniqueElement
-    
-    numPixel = size(find(img == double(uniqueElement(i))), 1);
-    
-    functionValue = numPixel/numElement;
-    
-    numPixelList = [numPixelList; functionValue];
-    
+for i = 1 : numUniqueElement   
+    numPixel = size(find(img == double(uniqueElement(i))), 1);    
+    functionValue = numPixel/numElement;    
+    numPixelList = [numPixelList; functionValue];    
 end
 
 % compute cumulative probability funcition
-
 cumuLativePixel = cumsum(numPixelList);
-
 if cumuLativePixel(end) ~= 1
     error(message(('alogrithm histogram equalization is wrong')))
 end
@@ -60,44 +49,29 @@ if method == 1
     %Methond 1 (common)
     %compute the pixel mapping
     mappingElementList = [];
-
     for i = 1: numUniqueElement
-
         mappingElement = ceil(cumuLativePixel(i)*(high-low + 1) -1);
-
         mappingElementList = [mappingElementList; mappingElement];
-
-    end
-    
+    end    
 end
 
-if method == 2
-    
+if method == 2   
     % Method 2 (Histogram Hyperbolisation)
     % compute the pixel mapping
     mappingElementList = [];
-
     A = alpha/(1 - exp(-alpha*(high-low)));
-
     for i = 1: numUniqueElement
-
     mappingElement = floor((-1/alpha)* log(1 - (alpha/A)*cumuLativePixel(i)) + 0.5); 
-
     mappingElementList = [mappingElementList; mappingElement];
-
     end
 end
 
 % get the transfered image
-
 HEImg = zeros(size(img));
 
-for i = 1:numUniqueElement
-    
-    index = find(img == uniqueElement(i));
-    
-    HEImg(index) = mappingElementList(i);
-    
+for i = 1:numUniqueElement    
+    index = find(img == uniqueElement(i));    
+    HEImg(index) = mappingElementList(i);    
 end
 
 processedImg = uint8(HEImg);
